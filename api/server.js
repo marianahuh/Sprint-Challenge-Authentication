5796d08be099904914684b3c01a79a0276d61e92
Mariana Huh <mhuh0176@gmail.com>
2019-08-30 18:24:33 -0700

    config file

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const jwt = require('jsonwebtoken');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -14,5 +15,26 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+  res.send("It's alive! Muuhahaaa!");
+});
+
+server.get('/token', (req, res) => {
+  const payload = {
+    subject: 'jokes',
+    username: 'dad',
+    favoriteDay: 'Holiday'
+  };
+  const secret = 'thesecret';
+  const options = {
+    expiresIn: '1h'
+  };
+
+  const token = jwt.sign(payload, secret, options);
+  console.log(token);
+
+  res.json(token);
+});
 
 module.exports = server;
